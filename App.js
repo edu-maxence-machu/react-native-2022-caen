09f74326ccf1a41b89751dd8e1f1f4449a4abdc8
Maxence Machu <maxence@akarah.com>
2022-04-07 09:09:48 +0200

    add(demo): style

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={{flex: 1}}>{/* 
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
+
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      />
+
+        <Button
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        /> */}
+
+        <View style={styles.container}>
+          <View style={styles.helloContainer}>
+            <Text style={styles.helloText}>HELLO WORLD</Text>
+          </View>
+        </View>
+        
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  helloContainer: {
+      width: '100%',
+      backgroundColor: '#104360',
+      paddingVertical: 20,
+      borderBottomLeftRadius: 0,
+      borderTopRightRadius: 30,
+      borderBottomLeftRadius: 30
+  }, 
+  helloText: {
+    fontSize: 46,
+    fontWeight: "900",
+    textAlign: "center",
+    letterSpacing: 5,
+    color: '#fff',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'grey',
+    padding: 20,
   },
+  tinyLogo : {
+    width: 100,
+    height: 100
+  }
 });
